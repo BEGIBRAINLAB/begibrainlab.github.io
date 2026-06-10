308f82c9673df788a414e0c7aba99b9cbf189a6f
BEGIBRAINLAB <contact@begibrainlab.com>
2026-06-10 02:09:32 +0200

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,19 @@
-(function(){
-  const y=document.getElementById('year'); if(y) y.textContent=new Date().getFullYear();
-})();
+const yearElement = document.getElementById("year");
+
+if (yearElement) {
+  yearElement.textContent = new Date().getFullYear();
+}
+
+const heroSlides = document.querySelectorAll(".hero-slide");
+
+if (heroSlides.length > 1) {
+  let currentSlide = 0;
+
+  setInterval(() => {
+    heroSlides[currentSlide].classList.remove("active");
+
+    currentSlide = (currentSlide + 1) % heroSlides.length;
+
+    heroSlides[currentSlide].classList.add("active");
+  }, 4500);
+}
